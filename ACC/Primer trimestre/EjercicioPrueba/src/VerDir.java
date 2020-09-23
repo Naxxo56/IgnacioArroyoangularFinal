070cf7c4288e6d90efa6df52a826979463d96b28
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-09-23 14:43:32 +0200

    fichero

---
+++ ACC/Primer trimestre/EjercicioPrueba/src/VerDir.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class VerDir {
+    public static void main(String[] args) {
+        String dir = ".";
+        File f = new File(dir);
+        String[] archivos = f.list();
+        System.out.printf("Ficheros en mi directorio actual: %d %n", archivos.length);
+        for (int i = 0; i < archivos.length; i++) {
+            File f2 = new File(f, archivos[i]);
+            System.out.printf("Nombre %s, ¿Es fichero?: %b, ¿Es directorio?" +
+                    " %b %n", archivos[i], f2.isFile(), f2.isDirectory());
+        }
+
+
+    }
+}
--- ACC/Primer trimestre/EjercicioPrueba/src/VerDir_b.java
+++ ACC/Primer trimestre/EjercicioPrueba/src/VerDir_b.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class VerDir_b {
     public static void main(String[] args) {
         System.out.println("Ficheros en el directorio actual");
-        File dir = new File(".");
+        File dir = new File("C:\\Users\\digim\\OneDrive\\Escritorio\\Fotos");
         String [] archivos = dir.list();
        for (int i = 0; i < archivos.length ; i++) {
             System.out.println(archivos[i]);
