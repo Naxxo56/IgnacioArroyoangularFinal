9f869d7e5584ae2549748d6a689e535537108477
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-09-30 13:17:48 +0200

    organizado pmdm a DI

---
+++ DI/Primer Trimestre/T01_Inicio/src/module-info.java
@@ -0,0 +1,10 @@
+module T01.Inicio {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+}
