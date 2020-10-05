223e5b68cac65de2d82cc156a162b3b053a247a8
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-05 14:44:54 +0200

    ACC 05-10

---
+++ ACC/Primer trimestre/EjercicioPrueba/src/CrearEmpleadoXML.java
@@ -0,0 +1,26 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class CrearEmpleadoXML {
+    public static void main(String[] args) throws IOException {
+        File fichero = new File("C:\\Users\\digim\\Documents\\auxiliar_java\\Aleatorio.dat");
+        if (!fichero.exists()) {
+            fichero.createNewFile();
+        }
+        RandomAccessFile rafile = new RandomAccessFile(fichero, "r");
+        int id, departamento;
+        double salario;
+        char[] nombre = new char[10];
+        char aux;
+        String apellido;
+        //Acceso aleatorio.Variable para situar el puntero del fichero inicializando a 0 para el inicio.
+        long posicion = 0;
+        //Creamos una instancia de DocumentBuilderFactory para construir el parser.
+        //Puede producir una excepcion ParserConfigurationException.
+        DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
+
+    }
+}
