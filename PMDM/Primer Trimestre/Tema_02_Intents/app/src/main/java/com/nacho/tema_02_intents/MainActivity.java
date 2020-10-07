d34753d0e0356af4949434887f2eb30f5632c838
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-07 15:28:34 +0200

    final pmdm 07-10

--- PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/MainActivity.java
+++ PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/MainActivity.java
@@ -1,4 +1,4 @@
-package  com.nacho.tema_02_intents;
+package com.nacho.tema_02_intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.nacho.tema_02_intents.utils.Persona;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button botonArranque, botonDatos;
+    private Button botonArranque, botonDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         botonArranque.setOnClickListener(this);
     }
 
-    public  void instancias(){
+    public void instancias() {
         botonArranque = findViewById(R.id.boton_arrancar_pantalla);
         botonDatos = findViewById(R.id.boton_arrancar_pantalla_dato);
 
@@ -33,14 +35,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        Intent intent = null;
+        switch (view.getId()) {
             case R.id.boton_arrancar_pantalla:
-                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
-                startActivity(intent);
+                intent= new Intent(getApplicationContext(),SegundaActivity.class);
                 break;
-            case  R.id.boton_arrancar_pantalla_dato:
-
+            case R.id.boton_arrancar_pantalla_dato:
+                intent= new Intent(getApplicationContext(),SegundaActivity.class);
+                /*
+                intent.putExtra("nombre","Nacho");
+                intent.putExtra("apellido","Arroyo");
+                intent.putExtra("edad",20);
+                intent.putExtra("experiencia",true);
+                 */
+                intent.putExtra("persona", new Persona("Nacho ","Arroyo ",20 , true ));
                 break;
         }
+        startActivity(intent);
     }
 }
--- PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/SegundaActivity.java
+++ PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/SegundaActivity.java
@@ -2,14 +2,40 @@ package com.nacho.tema_02_intents;
 
 import android.app.AppComponentFactory;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.nacho.tema_02_intents.utils.Persona;
+
 public class SegundaActivity extends AppCompatActivity {
+    private TextView texto;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_activity);
+        instancias();
+        if(getIntent().getExtras()!=null){
+            recuperarDatos();
+        }
     }
+
+    private void instancias() {
+        texto = findViewById(R.id.texto_pantalla_dos);
+    }
+
+    private void recuperarDatos() {
+        //el bundle es como la caja con los datos
+        Bundle bundle = getIntent().getExtras();
+        /*
+        String nombreRecuperado = bundle.getString("nombre");
+        String apellidoRecuperado = bundle.getString("apellido");
+        int edadRecuperada = bundle.getInt("edad");
+        Boolean experienciaRecuperada = bundle.getBoolean("experiencia");
+         */
+        Persona persona = (Persona) bundle.getSerializable("persona");
+        texto.setText(persona.getNombre()+""+persona.getApellido()+""+persona.getEdad()+""+persona.isExperiencia());
+    }
+
 }
