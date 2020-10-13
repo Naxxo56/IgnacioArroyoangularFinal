470cf537c5342d63670dd14b9d13f70a555354ac
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-13 14:51:13 +0200

    DI Y PMDM 13/10

--- DI/Primer Trimestre/T02_Pestaniass/src/sample/ControladoraInicial.java
+++ DI/Primer Trimestre/T02_Pestaniass/src/sample/ControladoraInicial.java
@@ -1,4 +1,112 @@
 package sample;
 
-public class ControladoraInicial {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import sample.utils.Persona;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladoraInicial implements Initializable {
+    @FXML
+    Button botonImagen, botonCambio;
+    @FXML
+    RadioButton radioUno, radioDos, radioTres, radioCuatro;
+
+    DropShadow sombraExterior;
+
+    ToggleGroup grupoRadios;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        instancias();
+        personalizarBotones();
+        acciones();
+    }
+
+    private void instancias() {
+        sombraExterior = new DropShadow();
+        grupoRadios = new ToggleGroup();
+        asociarDatos();
+        grupoRadios.getToggles().addAll(radioUno, radioDos, radioTres, radioCuatro);
+    }
+
+    private void asociarDatos() {
+        radioUno.setUserData(new Persona("Nacho","Arroyo",12345));
+        radioDos.setUserData(new Persona("Ernesto","Gaspar",123456));
+        radioTres.setUserData(new Persona("Roberto","Reviriego",1234567));
+        radioCuatro.setUserData(new Persona("Victor","Pellicer",12345678));
+    }
+
+    private void acciones() {
+        botonImagen.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
+        botonImagen.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
+        botonCambio.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
+        botonCambio.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
+
+        grupoRadios.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle oldValue, Toggle newValue) {
+                /*
+
+                System.out.println("Cambio de seleccion");
+                System.out.println(oldValue);
+                System.out.println(newValue);
+
+                 */
+                Persona personaRecuperada = (Persona)newValue.getUserData();
+                System.out.println(personaRecuperada.getNombre());
+            }
+        });
+
+
+    }
+
+    private void personalizarBotones() {
+        //Image imageOk=new Image(String.valueOf(getClass().getResource("resources/button_on.png")));
+        //botonImagen.setGraphic(new ImageView(imageOk));
+        botonImagen.setGraphic(new ImageView(new Image(String.valueOf(getClass().getResource("resources/button_on.png")))));
+        botonImagen.setBackground(null);
+        botonCambio.setGraphic(new ImageView(new Image(String.valueOf(getClass().getResource("resources/button_ok.png")))));
+        ;
+        botonCambio.setText("");
+        botonCambio.setBackground(null);
+        //botonImagen.setEffect(sombraExterior);
+    }
+
+    class ManejoRaton implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent event) {
+            //quien ha generado
+            //que ha generado
+            if (event.getSource() == botonImagen) {
+                if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+                    botonImagen.setEffect(sombraExterior);
+                } else if ((event.getEventType() == MouseEvent.MOUSE_EXITED)) {
+                    botonImagen.setEffect(null);
+                }
+            } else if (event.getSource() == botonCambio) {
+                if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+                    botonCambio.setGraphic(new ImageView
+                            (new Image(getClass().getResourceAsStream("resources/button_cancel.png"))));
+                } else if ((event.getEventType() == MouseEvent.MOUSE_EXITED)) {
+                    botonCambio.setGraphic(new ImageView
+                            (new Image(getClass().getResourceAsStream("resources/button_ok.png"))));
+                }
+            }
+        }
+    }
 }
--- PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/MainActivity.java
+++ PMDM/Primer Trimestre/Tema_02_Intents/app/src/main/java/com/nacho/tema_02_intents/MainActivity.java
@@ -1,9 +1,11 @@
 package com.nacho.tema_02_intents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,7 +14,7 @@ import com.nacho.tema_02_intents.utils.Persona;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button botonArranque, botonDatos;
+    private Button botonArranque, botonDatos, botonResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void acciones() {
         botonDatos.setOnClickListener(this);
         botonArranque.setOnClickListener(this);
+        botonResultado.setOnClickListener(this);
     }
 
     public void instancias() {
         botonArranque = findViewById(R.id.boton_arrancar_pantalla);
         botonDatos = findViewById(R.id.boton_arrancar_pantalla_dato);
+        botonResultado = findViewById(R.id.boton_arrancar_pantalla_resultado);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == 0) {
+            Bundle bundle = data.getExtras();
+            int numero = bundle.getInt("dato1");
+            boolean cierto = bundle.getBoolean("dato2");
+            Log.v("Arranque", "Arranque y resultado 0"
+                    + String.valueOf (numero) +  String.valueOf (cierto));
+        }
     }
 
     @Override
@@ -38,19 +53,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = null;
         switch (view.getId()) {
             case R.id.boton_arrancar_pantalla:
-                intent= new Intent(getApplicationContext(),SegundaActivity.class);
+                intent = new Intent(getApplicationContext(), SegundaActivity.class);
+                startActivity(intent);
                 break;
             case R.id.boton_arrancar_pantalla_dato:
-                intent= new Intent(getApplicationContext(),SegundaActivity.class);
+                intent = new Intent(getApplicationContext(), SegundaActivity.class);
                 /*
                 intent.putExtra("nombre","Nacho");
                 intent.putExtra("apellido","Arroyo");
                 intent.putExtra("edad",20);
                 intent.putExtra("experiencia",true);
                  */
-                intent.putExtra("persona", new Persona("Nacho ","Arroyo ",20 , true ));
+                intent.putExtra("persona", new Persona("Nacho ", "Arroyo ", 20, true));
+                startActivity(intent);
+                break;
+            case R.id.boton_arrancar_pantalla_resultado:
+                intent = new Intent(getApplicationContext(), TerceraActivity.class);
+                startActivityForResult(intent, 0);
                 break;
         }
-        startActivity(intent);
+
+
     }
 }
