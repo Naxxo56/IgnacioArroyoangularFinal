00c28b6b5b479d37e4198c8133dabfd5fa7cf20b
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-15 10:48:27 +0200

    ACC Y PMDM 15/10

--- DI/Primer Trimestre/T02_Pestaniass/src/sample/ControladoraInicial.java
+++ DI/Primer Trimestre/T02_Pestaniass/src/sample/ControladoraInicial.java
@@ -6,13 +6,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import sample.utils.Persona;
 
@@ -21,13 +20,25 @@ import java.util.ResourceBundle;
 
 public class ControladoraInicial implements Initializable {
     @FXML
-    Button botonImagen, botonCambio;
+    Button botonImagen, botonCambio, botonCapturaTexto;
+
     @FXML
-    RadioButton radioUno, radioDos, radioTres, radioCuatro;
+    RadioButton radio1, radio2, radio3, radio4;
+
+    @FXML
+    ToggleButton toggle1, toggle2;
+    @FXML
+    TextField textoNormal;
+    @FXML
+    TextArea textoArea;
+    @FXML
+    PasswordField textoPass;
+
+    @FXML
+    CheckBox check1;
 
     DropShadow sombraExterior;
-
-    ToggleGroup grupoRadios;
+    ToggleGroup grupoRadios, grupoToggles;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -39,73 +50,119 @@ public class ControladoraInicial implements Initializable {
     private void instancias() {
         sombraExterior = new DropShadow();
         grupoRadios = new ToggleGroup();
+        grupoToggles = new ToggleGroup();
         asociarDatos();
-        grupoRadios.getToggles().addAll(radioUno, radioDos, radioTres, radioCuatro);
+        grupoRadios.getToggles().addAll(radio1, radio2, radio3, radio4);
+        grupoToggles.getToggles().addAll(toggle1, toggle2);
     }
 
     private void asociarDatos() {
-        radioUno.setUserData(new Persona("Nacho","Arroyo",12345));
-        radioDos.setUserData(new Persona("Ernesto","Gaspar",123456));
-        radioTres.setUserData(new Persona("Roberto","Reviriego",1234567));
-        radioCuatro.setUserData(new Persona("Victor","Pellicer",12345678));
+        radio1.setUserData(new Persona("Borja", "Martin", 123213));
+        radio2.setUserData(new Persona("Jose", "Martin", 123213));
+        radio3.setUserData(new Persona("Roberto", "Martin", 123213));
+        radio4.setUserData(new Persona("David", "Martin", 123213));
     }
 
     private void acciones() {
+
         botonImagen.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
         botonImagen.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
         botonCambio.addEventHandler(MouseEvent.MOUSE_ENTERED, new ManejoRaton());
         botonCambio.addEventHandler(MouseEvent.MOUSE_EXITED, new ManejoRaton());
-
+        botonImagen.setOnAction(new ManejoPulsaciones());
+        botonCapturaTexto.setOnAction(new ManejoPulsaciones());
         grupoRadios.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
             @Override
             public void changed(ObservableValue<? extends Toggle> observableValue, Toggle oldValue, Toggle newValue) {
-                /*
-
-                System.out.println("Cambio de seleccion");
+                /*System.out.println("cambio");
                 System.out.println(oldValue);
-                System.out.println(newValue);
-
-                 */
-                Persona personaRecuperada = (Persona)newValue.getUserData();
-                System.out.println(personaRecuperada.getNombre());
+                System.out.println(newValue);*/
+                Persona personaRecuerada = (Persona) newValue.getUserData();
+                System.out.println(personaRecuerada.getNombre());
+            }
+        });
+        grupoToggles.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observableValue, Toggle toggle, Toggle t1) {
+                System.out.println("cambio");
+            }
+        });
+        check1.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean t1) {
+                //System.out.println(t1);
+                botonImagen.setDisable(t1);
             }
         });
 
+        textoNormal.addEventFilter(KeyEvent.KEY_TYPED,new ManejoTeclas());
 
     }
 
     private void personalizarBotones() {
-        //Image imageOk=new Image(String.valueOf(getClass().getResource("resources/button_on.png")));
-        //botonImagen.setGraphic(new ImageView(imageOk));
-        botonImagen.setGraphic(new ImageView(new Image(String.valueOf(getClass().getResource("resources/button_on.png")))));
+
+        Image imagenOk = new Image(getClass().getResourceAsStream("resources/button_on.png"));
+        botonImagen.setGraphic(new ImageView(imagenOk));
         botonImagen.setBackground(null);
-        botonCambio.setGraphic(new ImageView(new Image(String.valueOf(getClass().getResource("resources/button_ok.png")))));
-        ;
+        botonCambio.setGraphic(new ImageView
+                (new Image(getClass().getResourceAsStream("resources/button_ok.png"))));
         botonCambio.setText("");
         botonCambio.setBackground(null);
         //botonImagen.setEffect(sombraExterior);
     }
 
+    class ManejoTeclas implements EventHandler<KeyEvent>{
+
+        @Override
+        public void handle(KeyEvent keyEvent) {
+
+        }
+    }
     class ManejoRaton implements EventHandler<MouseEvent> {
 
         @Override
         public void handle(MouseEvent event) {
-            //quien ha generado
-            //que ha generado
+
+            // quien ha generado
+            // que ha generado
             if (event.getSource() == botonImagen) {
                 if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
                     botonImagen.setEffect(sombraExterior);
-                } else if ((event.getEventType() == MouseEvent.MOUSE_EXITED)) {
+                } else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
                     botonImagen.setEffect(null);
                 }
             } else if (event.getSource() == botonCambio) {
                 if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
                     botonCambio.setGraphic(new ImageView
                             (new Image(getClass().getResourceAsStream("resources/button_cancel.png"))));
-                } else if ((event.getEventType() == MouseEvent.MOUSE_EXITED)) {
+                    botonCambio.setEffect(sombraExterior);
+                } else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
                     botonCambio.setGraphic(new ImageView
                             (new Image(getClass().getResourceAsStream("resources/button_ok.png"))));
+                    botonCambio.setEffect(null);
                 }
+            }
+
+            // que evento ha generado
+            // quien lo ha hecho
+
+        }
+    }
+
+    class ManejoPulsaciones implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            if (actionEvent.getSource() == botonImagen) {
+                System.out.println(grupoRadios.getSelectedToggle());
+            } else if (actionEvent.getSource() == botonCapturaTexto) {
+                /*
+                System.out.println(textoNormal.getText());
+                System.out.println(textoArea.getText());
+                System.out.println(textoPass.getText());
+                 */
+                textoNormal.appendText("s");
+                String numero = textoNormal.getText();
             }
         }
     }
