13c1e043fc3c8f009c111fec7ed40bafa92d42ef
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-27 14:43:55 +0100

    cartas 27-10

--- PMDM/Primer Trimestre/Proyecto_Cartas/app/src/main/java/com/example/proyecto_cartas/JuegoActivity.java
+++ PMDM/Primer Trimestre/Proyecto_Cartas/app/src/main/java/com/example/proyecto_cartas/JuegoActivity.java
@@ -2,12 +2,22 @@ package com.example.proyecto_cartas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
-public class JuegoActivity extends AppCompatActivity {
-    private Button botonAbajoNo, botonAbajoPulsado, botonArribaPulsado, botonArribaNo;
+public class JuegoActivity extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout fondo;
+    ImageButton botonMayor, botonMenor;
+    int[] cartas;
+    int aleatorio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +25,54 @@ public class JuegoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_juego);
         instancias();
         acciones();
+        aleatorio = (int) (Math.random() * 12);
+        //Log.v("aleatorio", String.valueOf(aleatorio));
+        fondo.setBackgroundResource(cartas[aleatorio]);
+
     }
 
     private void acciones() {
+        botonMayor.setOnClickListener(this);
+        botonMenor.setOnClickListener(this);
     }
 
     private void instancias() {
+        fondo = findViewById(R.id.fondo_cartas);
+        cartas = new int[]{R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4,
+                R.drawable.c5, R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9,
+                R.drawable.c10, R.drawable.c11, R.drawable.c12, R.drawable.c13};
+        botonMayor = findViewById(R.id.boton_mayor);
+        botonMayor = findViewById(R.id.boton_menor);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int aleatorioNuevo = (int) (Math.random() * 12);
+        switch(v.getId()){
+            case R.id.boton_menor:
+                if(aleatorio>aleatorioNuevo){
+                    fondo.setBackgroundResource(cartas[aleatorioNuevo]);
+                    aleatorio=aleatorioNuevo;
+
+                }else{
+                    finish();
+                }
+
+                break;
+            case R.id.boton_mayor:
+                if(aleatorio<aleatorioNuevo){
+                    fondo.setBackgroundResource(cartas[aleatorioNuevo]);
+                    aleatorio=aleatorioNuevo;
+
+                }else{
+                    Intent intent = new Intent();
+                    intent.putExtra("valor",2);
+                    setResult(0,intent);
+                    finish();
+                }
+                break;
+        }
     }
 }
--- PMDM/Primer Trimestre/Proyecto_Cartas/app/src/main/java/com/example/proyecto_cartas/MainActivity.java
+++ PMDM/Primer Trimestre/Proyecto_Cartas/app/src/main/java/com/example/proyecto_cartas/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.proyecto_cartas;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  {
-    private Button botonJugar;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button botonIniciar;
     private CheckBox checkPracticar;
+    private TextView record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +24,30 @@ public class MainActivity extends AppCompatActivity  {
     }
 
     private void acciones() {
-        botonJugar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        botonIniciar.setOnClickListener(this);
 
-            }
-        });
     }
 
     private void instancias() {
-        botonJugar=findViewById(R.id.botonJugar);
+        botonIniciar=findViewById(R.id.botonJugar);
         checkPracticar=findViewById(R.id.checkPracticar);
+        record = findViewById(R.id.texto_record);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(),JuegoActivity.class);
+        int codigo;
+        if(checkPracticar.isChecked()){
+            codigo=0;
+        }else{
+            codigo=1;
+        }
+        startActivityForResult(intent,codigo);
     }
 }
