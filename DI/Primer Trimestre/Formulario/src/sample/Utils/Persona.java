7364cd5c14c33f1fc62e39cb905a85a47ca61146
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-10-27 12:52:22 +0100

    DI FORMULARIO 27/10

---
+++ DI/Primer Trimestre/Formulario/src/sample/Utils/Persona.java
@@ -0,0 +1,67 @@
+package sample.Utils;
+
+public class Persona {
+    String nombre, apellido, estado;
+    int telefono;
+    boolean disponibilidad;
+
+    public Persona() {
+
+    }
+
+    public Persona(String nombre, String apellido, int telefono, boolean disponibilidad, String estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.disponibilidad = disponibilidad;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+}
