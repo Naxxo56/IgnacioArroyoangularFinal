3d6c0cba5934157ffd613737eb5d8058a2c76818
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-11-18 18:36:26 +0100

    Splash PMDM

---
+++ PMDM/Primer Trimestre/VersionesActivity/app/src/main/java/com/example/versionesactivity/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.versionesactivity;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
