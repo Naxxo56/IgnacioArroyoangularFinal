e54f88db516ddafea49b168c9b2414bb0dd95aec
Naxxo56 <55828093+Naxxo56@users.noreply.github.com>
2020-11-19 10:56:20 +0100

    JSON DI

---
+++ DI/Primer Trimestre/T02_ComunicacionJSON/src/module-info.java
@@ -0,0 +1,11 @@
+module T02.ComunicacionJSON {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.json;
+    requires gson;
+
+    opens sample;
+
+}
